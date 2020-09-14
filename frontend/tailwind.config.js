1e117e2a361f91ca9b28651716fee6cf8de7738b
AbdoMgdy <abdoman46@hotmail.com>
2020-09-15 00:19:53 +0200

    add initial tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
+    removeDeprecatedGapUtilities: true
     // purgeLayersByDefault: true
   },
   purge: {
@@ -12,6 +12,26 @@ module.exports = {
   },
   // ...
   theme: {
+    fontFamily: {
+      display: ['Rubik', 'PT Serif', 'sans-serif'],
+      body: ['Rubik', 'sans-serif']
+    },
+    color: {
+      c100: '#f9320c',
+      c200: '#141f32',
+      c300: '#7a8baa',
+      c400: '#f9f7ff'
+    },
+    fontSize: {
+      sm: '0.875rem',
+      base: '1rem',
+      md: '1.125rem',
+      lg: '1.5rem',
+      xlg: '1.875rem',
+      xxlg: '3.125rem'
+    },
+    //fontWeigt
+    //lineHeight
     extend: {}
   },
   variants: {},
