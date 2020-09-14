99803790b99940796cfc05567c3ab024fa1c5cf1
Ahmed Ayoub <great.nile@gmail.com>
2020-09-14 18:27:22 +0200

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { API } from './modules/apis';
+
+export default class App extends React.Component {
+  state = {
+    navigation: []
+  };
+  componentDidMount() {
+    API('/navigation').then(({ data: navigation }) => {
+      this.setState({ navigation });
+    });
+  }
+  render() {
+    return this.state.navigation.map(({ title }) => <div>{title}</div>);
+  }
+}
