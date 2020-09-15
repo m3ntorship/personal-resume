fa5b4160846e26e7e08bf3296effc238a7b7ca01
MAlngaawy <m.alngaawy@gmail.com>
2020-09-15 14:34:07 +0200

    add the three different buttons style to the test component to review as prototype

--- frontend/src/components/TestComponent/index.jsx
+++ frontend/src/components/TestComponent/index.jsx
@@ -7,7 +7,9 @@ export const TestComponent = () => {
       <h1> Shared Components Prototype </h1>
       <div>
         <h3> Buttons </h3>
-        <Button Children='First One' />
+        <Button Children='First One' rounded={true} customClassNames='mt-20 block' />
+        <Button Children='Second One' rounded={true} customClassNames='mt-10 block' bgColor="red" />
+        <Button Children='Third One' rounded={true} customClassNames='mt-15 block' bgColor="blue" />
       </div>
     </div>
 
