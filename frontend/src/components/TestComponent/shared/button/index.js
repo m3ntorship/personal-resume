169631b4ea3678987d251c3b773ed6ede3f1a534
MAlngaawy <m.alngaawy@gmail.com>
2020-09-15 21:48:16 +0200

    add final styles and options to the component

--- frontend/src/components/TestComponent/shared/button/index.js
+++ frontend/src/components/TestComponent/shared/button/index.js
@@ -1,21 +1,19 @@
-import React, { Children } from 'react';
-import './style.css';
+import React from 'react';
 import cn from 'classnames';
 
-// Header Buttons (W:230, H:65)
-// about Buttons (W:220, H:65)
-
-export const Button = ({ Children, customClassNames, rounded, bgColor }) => {
+export const Button = ({ children, customClassNames, rounded, bgColor }) => {
   return (
     <button
       className={cn(
-        'my-10 mx-auto uppercase bg-transparent text-gray-800 border-solid border-4 border-red-200 custom_styles',
+        'my-10 mx-auto uppercase text-sm font-regular text-c200 bg-transparent border-solid border-4 border-c100 border-opacity-25 custom_styles',
         { 'rounded-full': rounded },
-        { ['border-none text-red-100 bg__' + bgColor]: bgColor },
+        { ['border-none text-white bg-' + bgColor]: bgColor },
         customClassNames
       )}
     >
-      {Children}
+      {children}
     </button>
   );
 };
+
+// Buttons Widths and heights
