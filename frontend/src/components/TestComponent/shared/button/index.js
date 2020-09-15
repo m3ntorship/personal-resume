9a548366d4eedd66103ba5f10a4efae22f9c9732
MAlngaawy <m.alngaawy@gmail.com>
2020-09-15 14:32:26 +0200

    initialize some button props and use it with classnames package to handle defrent styles

--- frontend/src/components/TestComponent/shared/button/index.js
+++ frontend/src/components/TestComponent/shared/button/index.js
@@ -1,13 +1,21 @@
 import React, { Children } from 'react';
-import './style.css'
+import './style.css';
+import cn from 'classnames';
 
 // Header Buttons (W:230, H:65)
 // about Buttons (W:220, H:65)
 
-export const Button = ({ Children }) => {
-    return (
-        <button className="bg-red-600 my-10 mx-auto text-white custom_styles">
-            {Children}
-        </button>
-    )
-}
+export const Button = ({ Children, customClassNames, rounded, bgColor }) => {
+  return (
+    <button
+      className={cn(
+        'my-10 mx-auto uppercase bg-transparent text-gray-800 border-solid border-4 border-red-200 custom_styles',
+        { 'rounded-full': rounded },
+        { ['border-none text-red-100 bg__' + bgColor]: bgColor },
+        customClassNames
+      )}
+    >
+      {Children}
+    </button>
+  );
+};
