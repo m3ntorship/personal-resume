b18f68b54fdc58420d9ca3a8ad52a817885aba28
mina bessada <mina@wizholding.com>
2020-09-16 10:15:33 +0200

    update style images , heading and paragraph

--- frontend/src/components/GalleryComponent/Gallery.jsx
+++ frontend/src/components/GalleryComponent/Gallery.jsx
@@ -9,29 +9,37 @@ import woman from '../../images/img-young-woman-thinking.png';
 const Gallery = () => {
   return (
     <React.Fragment>
-      <div className="container mx-auto">
-        <div>
+      <div className="container mx-auto ">
+        <div className="mb-16 text-center ">
           <h2>My Recent Works </h2>
         </div>
-        <div></div>
-        <div className=" grid grid-rows-3 gap-4 grid-cols-3 ">
-          <div>
-            <img src={art} alt="" />
+        <div className="mb-16 text-center">
+          <p>
+            <span className="uppercase  inline-block m-5 ">all works </span>
+            <span className="uppercase  inline-block m-5 ">ui/ux</span>
+            <span className="uppercase  inline-block m-5 ">branding</span>
+            <span className="uppercase  inline-block m-5 ">logo</span>
+            <span className="uppercase  inline-block m-5 ">development</span>
+          </p>
+        </div>
+        <div className=" grid grid-rows-2  grid-cols-3 gap-x-8 gap-y-6 ">
+          <div className="max-w-sm">
+            <img className="w-full" src={art} alt="" />
           </div>
-          <div>
-            <img src={battle} alt="" />
+          <div className="max-w-sm">
+            <img className="w-full" src={battle} alt="" />
           </div>
-          <div>
-            <img src={blackandwhite} alt="" />
+          <div className="max-w-sm">
+            <img className="w-full" src={blackandwhite} alt="" />
           </div>
-          <div>
-            <img src={graylaptop} alt="" />
+          <div className="max-w-sm">
+            <img className="w-full" src={graylaptop} alt="" />
           </div>
-          <div>
-            <img src={imac} alt="" />
+          <div className="max-w-sm">
+            <img className="w-full" src={imac} alt="" />
           </div>
-          <div>
-            <img src={woman} alt="" />
+          <div className="max-w-sm">
+            <img className="w-full" src={woman} alt="" />
           </div>
         </div>
       </div>
