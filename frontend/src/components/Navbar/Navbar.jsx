02ef5abb56b1dcd3989b1eb1a2645c642aa6ca30
eslamhesham <esl2mhesham@gmail.com>
2020-09-16 14:08:55 +0200

    Fetching API from the backend, Loop Menu items instead of static HTML, adjusting the styles, adding Download Icon

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,67 +1,47 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { API } from '../../modules/apis';
-import Logo from './Logo.png';
 import { Button } from '../shared/button';
-
+import Logo from './Logo.png';
+import Icon from './Icon.svg';
 export const Navbar = () => {
+  // Fetching API from the backend
   const [links, setLinks] = useState([]);
-
   useEffect(() => {
     API('/navigation').then(({ data: navigation }) => {
       setLinks(navigation);
     });
   }, []);
-  console.log(links);
   return (
-    <nav className="flex  items-center justify-between w-5/6  bg-white shadow-xl p-3 rounded-full m-auto">
+    <nav className="flex  items-center justify-between w-5/6  bg-white shadow-lg p-3 my-10 rounded-full m-auto">
       <div className="mx-12">
         <img src={Logo} alt="" className="w-16" />
       </div>
-      <ul className="flex flex-row items-center justify-between ml-48 text-xl">
-        <li>
-          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
-            HOME
-          </a>
-        </li>
-        <li>
-          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
-            ABOUT US
-          </a>
-        </li>
-        <li>
-          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
-            SERVICES
-          </a>
-        </li>
-        <li>
-          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
-            PROJECTS
-          </a>
-        </li>
-        <li>
-          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
-            TESTMONIALS
-          </a>
-        </li>
-        <li>
-          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
-            BOLG
-          </a>
-        </li>
-        <li>
-          <a href="#" className=" text-c300 p-5 hover:text-c100">
-            CONTACT
-          </a>
-        </li>
+      <ul className="flex flex-row items-center justify-between ml-56 text-xl">
+        {/* Looping Menu items */}
+        {links.map((link, index) => {
+          return (
+            <a
+              href="/"
+              key={index}
+              className=" text-c300 hover:text-c100 uppercase font-medium  m-4 "
+            >
+              {link.title}
+            </a>
+          );
+        })}
       </ul>
-      <div className="">
+      <div>
+        {/* Reusable button compnenet */}
         <Button
           rounded={true}
           customClassNames="w-64 h-20 font-medium my-0"
           bgColor="c100"
         >
           Download My CV
+          <span className="ml-2">
+            <img src={Icon} alt="Download" className="w-5 inline" />
+          </span>
         </Button>
       </div>
     </nav>
