eae9966008bee4e1e21f7cc61e676836b92c6000
eslamhesham <esl2mhesham@gmail.com>
2020-09-16 06:22:09 +0200

    Adjust the spaces like the psd

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { API } from '../../modules/apis';
 import Logo from './Logo.png';
-const Navbar = () => {
+import { Button } from '../shared/button';
+
+export const Navbar = () => {
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    API('/navigation').then(({ data: navigation }) => {
+      setLinks(navigation);
+    });
+  }, []);
+  console.log(links);
   // colors: {
   //   c100: '#f9320c', // red
   //   c200: '#141f32', // dark-blue
@@ -11,53 +23,56 @@ const Navbar = () => {
   //   c800: '#74fa5e'  // light-green
   // },
   return (
-    <nav className="flex  items-center justify-between w-5/6  bg-white shadow-xl p-3 my-8 rounded-full m-auto">
-      <div className="mx-8">
-        <img src={Logo} alt="" className="m-2 w-12" />
+    <nav className="flex  items-center justify-between w-5/6  bg-white shadow-xl p-3 rounded-full m-auto">
+      <div className="mx-12">
+        <img src={Logo} alt="" className="w-16" />
       </div>
-      <ul className="flex flex-row items-center justify-between">
+      <ul className="flex flex-row items-center justify-between ml-48 text-xl">
         <li>
-          <a href="#" className=" text-c300 p-3 hover:text-c100">
-            Home
+          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
+            HOME
           </a>
         </li>
         <li>
-          <a href="#" className=" text-c300 p-3 hover:text-c100">
-            About us
+          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
+            ABOUT US
           </a>
         </li>
         <li>
-          <a href="#" className=" text-c300 p-3 hover:text-c100">
-            Services
+          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
+            SERVICES
           </a>
         </li>
         <li>
-          <a href="#" className=" text-c300 p-3 hover:text-c100">
-            Projects
+          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
+            PROJECTS
           </a>
         </li>
         <li>
-          <a href="#" className=" text-c300 p-3 hover:text-c100">
-            Testmonials
+          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
+            TESTMONIALS
           </a>
         </li>
         <li>
-          <a href="#" className=" text-c300 p-3 hover:text-c100">
-            Blog
+          <a href="#" className=" text-c300 p-5 hover:text-c100 uppercase">
+            BOLG
           </a>
         </li>
         <li>
-          <a href="#" className=" text-c300 p-3 hover:text-c100">
-            Contact
+          <a href="#" className=" text-c300 p-5 hover:text-c100">
+            CONTACT
           </a>
         </li>
       </ul>
-      <div className="button self-end">
-        <button className="bg-c100 px-12 py-5 rounded-full m-0">
-          Downoal My cv
-        </button>
+      <div className="">
+        <Button
+          rounded={true}
+          customClassNames="w-64 h-20 font-medium my-0"
+          bgColor="c100"
+        >
+          Download My CV
+        </Button>
       </div>
     </nav>
   );
 };
-export default Navbar;
