c873393784209ce7c22c9e4faae70c78c84353d6
abdelrhman_ali <abdelrahman.ali.sayed27@gmail.com>
2020-09-17 00:02:21 +0200

    styling & add avatar and dots images

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,19 +1,44 @@
 import React from 'react'
-import { ReactComponent as Dots } from './assets/dots.svg';
-import avatar from './assets/avatar.png'
-import '../.././css/main.css'
+import dots from './assets/dots.png';
+import avatar from './assets/avata1r.png'
+import { Button } from './../shared/button/index.js'
 
 export const HeaderComponent = () => {
+  let avatarImg = {
+    backgroundImage: `url("${avatar}")`
+  }
   return (
-    <div className="border-b-2 border-c100 " >
-      <div className="w-main-container mx-auto ">
-        <span className=" text-base text-c100 " >I am</span>
-        <h1>Mark parker</h1>
-        <p>I am professional UI Designer and Web Developer. I am working i</p>
-        <button>my services</button>
-        <button>recent works</button>
-        <Dots />
-        <img src={avatar} alt="My Image" className=" right w-40 h-40  " />
+    <div className="bg-c400 border-b-2 border-c100 " >
+      <div className="w-main-container mx-auto flex flex-row pt-56 ">
+
+        <div className="mb-56 ">
+
+          <div className="  " >
+            <span className=" text-lg font-body-Rubik font-bold text-c100 mb-4 " >I am</span>
+            <h1 className=" text-xxlg font-bold font-headings mb-4 " >Mark Parker</h1>
+            <p className=" text-base font-body leading-1 w-4/5 mb-4 " >I am professional UI Designer and Web Developer. I am working
+          in UI Park as a UI/UX Designer.</p>
+          </div>
+
+
+          <div className="  " >
+            <Button rounded={true} customClassNames='w-56 h-16 mr-4 uppercase ' bgColor="c100" >web services</Button>
+            <Button rounded={true} customClassNames='w-56 h-16 uppercase ' bgColor="c200">recent works</Button>
+          </div>
+
+        </div>
+
+        <div className=" ml-4 mb-4  p-4 t ">
+          <div className="left-4 absolute z-0 top-10" >
+            <img src={dots} alt="" className=" " />
+          </div>
+          <div className=" z-10 " >
+            <img src={avatar} alt="" className=" w-full h-full rounded-full top-2 z-10  border-white border-8 right-0 " />
+          </div>
+        </div>
+
+
+
       </div>
     </div>
   )
