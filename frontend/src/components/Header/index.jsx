89cfd348b723728c4b0cda66bdce8b8fa76be6e8
abdelrhman_ali <abdelrahman.ali.sayed27@gmail.com>
2020-09-17 19:02:06 +0200

    adjust dots , add dots and avatar new photos

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,45 +1,41 @@
 import React from 'react'
-import dots from './assets/dots.png';
-import avatar from './assets/avata1r.png'
+import dots from './assets/dots3.png';
+import avatar from './assets/avatar2.png'
 import { Button } from './../shared/button/index.js'
 
 export const HeaderComponent = () => {
-  let avatarImg = {
-    backgroundImage: `url("${avatar}")`
-  }
   return (
-    <div className="bg-c400 border-b-2 border-c100 " >
-      <header className="w-main-container mx-auto flex flex-row pt-56 ">
+    <header className=" w-full border-c100 border-b-4 bg-c400 " >
+      <div className="container flex flex-row pt-32 relative  ">
 
-        <div className="mb-56 ">
+        <div className="w-1/2 mb-48 mt-8 ">
 
           <div className="  " >
-            <span className=" text-lg font-body-Rubik font-bold text-c100 mb-4 " >I am</span>
-            <h1 className=" text-xxlg font-bold font-headings mb-4 " >Mark Parker</h1>
-            <p className=" text-base font-body leading-1 w-4/5 mb-4 " >I am professional UI Designer and Web Developer. I am working
+            <span className=" text-c100 font-body text-xlg font-medium leading-1 ">I am</span>
+            <h1 className=" text-c200  text-xxlg font-headings" >Mark Parker</h1>
+            <p className="text-c300 font-body leading-1 mb-12 " >I am professional UI Designer and Web Developer. I am working
           in UI Park as a UI/UX Designer.</p>
           </div>
 
 
           <div className="  " >
-            <Button rounded={true} customClassNames='w-56 h-16 mr-4 uppercase ' bgColor="c100" >web services</Button>
-            <Button rounded={true} customClassNames='w-56 h-16 uppercase ' bgColor="c200">recent works</Button>
+            <Button rounded={true} customClassNames='w-56 h-16 mr-4' bgColor="c100" >web services</Button>
+            <Button rounded={true} customClassNames='w-56 h-16' bgColor="c200">recent works</Button>
           </div>
 
         </div>
 
-        <div className=" ml-4 mb-4  p-4 t ">
-          <div className="left-4 absolute z-0 top-10" >
-            <img src={dots} alt="" className=" " />
-          </div>
-          <div className=" z-10 " >
-            <img src={avatar} alt="" className=" w-full h-full rounded-full top-2 z-10  border-white border-8 right-0 " />
-          </div>
+
+        <div className="w-1/2 ">
+          <img src={dots} alt="" className=" absolute w-64  mb-32 ml-6 -top-20 transform  translate-x-4 " />
+          <img src={avatar} alt="" className=" relative rounded-full border-8 border-white ml-16 " />
         </div>
 
 
 
-      </header>
-    </div>
+
+      </div>
+
+    </header>
   )
 }
