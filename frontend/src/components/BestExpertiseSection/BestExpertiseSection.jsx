d6a271a9e6dcd8dc20ba21f405ec72e0cccedc56
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-09-17 19:05:36 +0200

    finalize services section - completed requested changes

--- frontend/src/components/BestExpertiseSection/BestExpertiseSection.jsx
+++ frontend/src/components/BestExpertiseSection/BestExpertiseSection.jsx
@@ -3,31 +3,34 @@ import Card from './Card';
 import { API } from '../../modules/apis';
 
 const BestExpertiseSection = () => {
-  const [services, setServices] = useState([]);
+  const [services, setServices] = useState(null);
   useEffect(() => {
-    API('/services').then(({ data: services }) => {
-      setServices(services);
+    API('/services').then(({ data }) => {
+      setServices(data);
     });
   }, []);
-  console.log(services);
-  const cards = services.map(({ title, details, button }) => {
+  if (services) {
+    const cards = services.cards.map(({ title, details, button }) => {
+      return (
+        <Card
+          title={title}
+          details={details}
+          button={button}
+          key={Math.random()}
+        />
+      );
+    });
     return (
-      <Card
-        title={title}
-        details={details}
-        button={button}
-        key={Math.random()}
-      />
+      <section className="container text-center">
+        <h2 className="font-headings text-xxlg text-c200 my-10">
+          {services.title}
+        </h2>
+        <div className="grid grid-cols-3 gap-8">{cards}</div>
+      </section>
     );
-  });
-  return (
-    <section className="container text-center">
-      <h2 className="font-headings text-xxlg text-c200 my-10">
-        My Best Expertise
-      </h2>
-      <div className="grid grid-cols-3 gap-8">{cards}</div>
-    </section>
-  );
+  }else{
+    return <div>Cannot get data from server</div>
+  }
 };
 
 export default BestExpertiseSection;
