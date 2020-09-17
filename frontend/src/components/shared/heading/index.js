efee1394f60176d10cf6352d9d1de9d099c4a8e1
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-09-17 19:08:02 +0200

    removed unnecessary brackets

--- frontend/src/components/shared/heading/index.js
+++ frontend/src/components/shared/heading/index.js
@@ -16,11 +16,11 @@ export const Heading = ({
         {
           ['text-' + color]: color,
           ['font-' + fontWeight]: fontWeight,
-          ['text-lg']: fontSize === 'xsmall', //xsmall = 1.5rem
-          ['text-xlg']: fontSize === 'small', //small = 1.875rem
-          ['text-xxlg']: fontSize === 'medium', //medium = 3.125rem
-          ['text-xxxlg']: fontSize === 'large', //large = 6.25rem
-          ['leading-3']: largeLineHeight  //lineHeight = 4.375rem
+          'text-lg': fontSize === 'xsmall', //xsmall = 1.5rem
+          'text-xlg': fontSize === 'small', //small = 1.875rem
+          'text-xxlg': fontSize === 'medium', //medium = 3.125rem
+          'text-xxxlg': fontSize === 'large', //large = 6.25rem
+          'leading-3': largeLineHeight  //lineHeight = 4.375rem
         },
         customClassNames
       )}
