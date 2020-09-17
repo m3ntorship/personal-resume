9995ef1e7d173d356ad7b2a378fe3dbab3fa7b01
Alaa <Alaa.Saedy.eg@gmail.com>
2020-09-17 22:20:51 +0200

    adding section data to DB and use hooks to fetch it to the component then using it

--- frontend/src/components/EstimateSection/index.jsx
+++ frontend/src/components/EstimateSection/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Button } from '../shared/button';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { API } from '../../modules/apis';
 
 const EstimateSection = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    API('/estimatesection').then(({ data: estimatesection }) => {
+      setData(estimatesection);
+    });
+  });
+
+  const { subTitle, sectionTitle, btnLabel } = data;
+
   return (
     <section
       className="py-32 text-c200"
@@ -15,16 +25,16 @@ const EstimateSection = () => {
       }}
     >
       <div className="container">
-        <span className="text-lg">Estimate your project!</span>
+        <span className="text-lg">{sectionTitle}</span>
         <h6 className="text-xxlg my-12" style={{ fontFamily: 'PT serif' }}>
-          Have Any Project On Mind
+          {subTitle}
         </h6>
         <Button
           bgColor="c100"
           rounded={true}
           customClassNames="my-4 block w-56 h-16"
         >
-          get in touch
+          {btnLabel}
         </Button>
       </div>
     </section>
