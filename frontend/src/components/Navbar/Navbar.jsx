ef3098d0da1dcbba06934403a2fc8242cced5deb
eslamhesham <esl2mhesham@gmail.com>
2020-09-18 17:54:34 +0200

    Updated The state & data, Adding If condition & handle the error state, using unique id on the menu links, importing the button data from the back end

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,44 +6,57 @@ import Logo from './Logo.png';
 import Icon from './Icon.svg';
 export const Navbar = () => {
   // Fetching API from the backend
-  const [links, setLinks] = useState([]);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState();
   useEffect(() => {
-    API('/navigation').then(({ data: navigation }) => {
-      setLinks(navigation);
-    });
+    API('/navigation')
+      .then(({ data }) => {
+        setData(data);
+      })
+      .catch(error => {
+        setError(error.message);
+      });
   }, []);
-  return (
-    <nav className="flex  items-center justify-between w-5/6  bg-white shadow-lg p-3 my-10 rounded-full m-auto">
-      <div className="mx-12">
-        <img src={Logo} alt="" className="w-16" />
-      </div>
-      <ul className="flex flex-row items-center justify-between ml-56 text-xl">
-        {/* Looping Menu items */}
-        {links.map((link, index) => {
-          return (
-            <a
-              href="/"
-              key={index}
-              className=" text-c300 hover:text-c100 uppercase font-medium  m-4 "
-            >
-              {link.title}
-            </a>
-          );
-        })}
-      </ul>
-      <div>
-        {/* Reusable button compnenet */}
-        <Button
-          rounded={true}
-          customClassNames="w-64 h-20 font-medium my-0"
-          bgColor="c100"
-        >
-          Download My CV
-          <span className="ml-2">
-            <img src={Icon} alt="Download" className="w-5 inline" />
-          </span>
-        </Button>
-      </div>
-    </nav>
-  );
+  if (data) {
+    console.log(data);
+    return (
+      <nav className="flex  items-center justify-between w-5/6  bg-white shadow-lg p-3 my-10 rounded-full m-auto">
+        <div className="mx-12">
+          <img src={Logo} alt="" className="w-16" />
+        </div>
+        <ul className="flex flex-row items-center justify-between ml-56 text-xl">
+          {error ? (
+            <p className="text-c100 text-xl"> Hi </p>
+          ) : (
+            data.linksList.map(({ title, linkUrl, id }, index) => {
+              return (
+                <a
+                  href={linkUrl}
+                  key={id}
+                  className=" text-c300 hover:text-c100 uppercase font-medium  m-4 "
+                >
+                  {title}
+                </a>
+              );
+            })
+          )}
+        </ul>
+        <div>
+          {/* Reusable button compnenet */}
+          <Button
+            rounded={true}
+            customClassNames="w-64 h-20 font-medium my-0"
+            bgColor="c100"
+          >
+            {data.downloadBtn}
+            <span className="ml-2">
+              <img src={Icon} alt="Download" className="w-5 inline" />
+            </span>
+          </Button>
+        </div>
+      </nav>
+    );
+  } else {
+    return 'Data broken';
+  }
 };
