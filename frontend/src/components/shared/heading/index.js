95fee8a13f1374b1b99b75e07321ab81595cc456
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-09-18 19:06:40 +0200

    removed unnnecessary brackets

--- frontend/src/components/shared/heading/index.js
+++ frontend/src/components/shared/heading/index.js
@@ -25,12 +25,12 @@ export const Heading = ({
   const classes = cn(
     'font-bold font-headings',
     {
-      ['text-lg']: fontSize === HEADING_OPTIONS.FONT_SIZES.XSMALL,
-      ['text-xlg']: fontSize === HEADING_OPTIONS.FONT_SIZES.SMALL,
-      ['text-xxlg']: fontSize === HEADING_OPTIONS.FONT_SIZES.MEDIUM,
-      ['text-xxxlg']: fontSize === HEADING_OPTIONS.FONT_SIZES.LARGE,
-      ['text-c200']: color === HEADING_OPTIONS.COLOR.PRIMARY,
-      ['text-c100']: color === HEADING_OPTIONS.COLOR.SECONDARY,
+      'text-lg': fontSize === HEADING_OPTIONS.FONT_SIZES.XSMALL,
+      'text-xlg': fontSize === HEADING_OPTIONS.FONT_SIZES.SMALL,
+      'text-xxlg': fontSize === HEADING_OPTIONS.FONT_SIZES.MEDIUM,
+      'text-xxxlg': fontSize === HEADING_OPTIONS.FONT_SIZES.LARGE,
+      'text-c200': color === HEADING_OPTIONS.COLOR.PRIMARY,
+      'text-c100': color === HEADING_OPTIONS.COLOR.SECONDARY,
       'leading-3': hasLineHeight
     },
     className
