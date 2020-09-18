c4a8b58fdac16297a961922c5c0a25337f92cb3e
eslamhesham <esl2mhesham@gmail.com>
2020-09-19 00:08:36 +0200

    Adding component to the App.js  & remove Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 // import EstimateSection from './components/EstimateSection';
 import { Prototype } from './components/TestComponent';
 // import { API } from './modules/apis';
-import { Navbar } from './components/Navbar';
+// import { Navbar } from './components/Navbar';
+import { UpdatedPosts } from './components/UpdatedPosts';
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <UpdatedPosts />
         <Prototype />
       </div>
     );
