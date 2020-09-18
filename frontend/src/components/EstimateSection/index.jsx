b4c88084463b06543ead703890e68903495a3d6c
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-09-19 00:52:19 +0200

    fixed mistake - removed comment

--- frontend/src/components/EstimateSection/index.jsx
+++ frontend/src/components/EstimateSection/index.jsx
@@ -27,7 +27,7 @@ const EstimateSection = () => {
       >
         <div className="container">
           <span className="text-lg">{title}</span>
-          <Heading className="my-5">{subTitle}</Heading>
+          <Heading as='h6' className="my-5">{subTitle}</Heading>
           <Button
             bgColor="c100"
             rounded={true}
