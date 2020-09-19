a704ae65e56cbe44d4b6902cfba6b384b349a12d
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-09-19 19:01:56 +0200

    fetch data from backend and  rename images

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,59 +1,71 @@
 import React from 'react';
-import dots from './assets/dots3.png';
-import avatar from './assets/avata1r.png';
 import { Button } from './../shared/button/index.js';
 import { Heading, HEADING_OPTIONS } from '../shared/heading';
+import { useEffect, useState } from 'react';
+import { API } from '../../modules/apis';
+
 // import { Navbar } from './../../components/Navbar/index.jsx';
-// import { useEffect } from 'react';
-// import { API } from './../../modules/apis';
 
 const Header = () => {
-  return (
-    <header className=" w-full  bg-c400  border-c100 border-b-4 pb-32">
-      <div className="container flex flex-row pt-64 relative">
-        <div className="  mt-16  ">
-          <div className="  ">
-            <span className=" text-c100 font-body text-xxlg font-medium leading-1 ">
-              I am
-            </span>
-            <Heading as="h1" fontSize={HEADING_OPTIONS.FONT_SIZES.LARGE}>
-              Mark Parker
-            </Heading>
-            <p className="text-c300 font-body leading-2 text-lg mb-8 ">
-              I am professional UI Designer and Web Developer. I am working in
-              UI Park as a UI/UX Designer.
-            </p>
+  const [data, setData] = useState();
+  useEffect(() => {
+    API('/header').then(({ data }) => {
+      setData(data);
+    });
+  }, []);
+  if (data) {
+    return (
+      <header className=" w-full  bg-c400  border-c100 border-b-4 pb-32">
+        <div className="container flex flex-row pt-64 relative">
+          <div className="  mt-16  ">
+            <div className="  ">
+              <span className=" text-c100 font-body text-xxlg font-medium leading-1 ">
+                I am
+              </span>
+              <Heading as="h1" fontSize={HEADING_OPTIONS.FONT_SIZES.LARGE}>
+                {data.heading}
+              </Heading>
+              <p className="text-c300 font-body leading-2 text-lg mb-8 ">
+                {data.paragraph}
+              </p>
+            </div>
+
+            <div className="  ">
+              <Button
+                rounded={true}
+                customClassNames="w-56 h-16 mr-4"
+                bgColor="c100"
+              >
+                {data.webServicesButton.title}
+              </Button>
+              <Button
+                rounded={true}
+                customClassNames="w-56 h-16"
+                bgColor="c200"
+              >
+                {data.recentWorkesButton.title}
+              </Button>
+            </div>
           </div>
 
-          <div className="  ">
-            <Button
-              rounded={true}
-              customClassNames="w-56 h-16 mr-4"
-              bgColor="c100"
-            >
-              web services
-            </Button>
-            <Button rounded={true} customClassNames="w-56 h-16" bgColor="c200">
-              recent works
-            </Button>
+          <div className="w-1/2 mb-8 pb-8 ">
+            <img
+              src={data.dotsUrl}
+              alt=""
+              className=" absolute  mb-32   transform  translate-x-4  -translate-y-8   "
+            />
+            <img
+              src={data.avatarUrl}
+              alt=""
+              className=" object-cover w-60 h-60 relative rounded-full border-8 border-white ml-16 z-10 "
+            />
           </div>
         </div>
-
-        <div className="w-1/2 mb-8 pb-8 ">
-          <img
-            src={dots}
-            alt=""
-            className=" absolute  mb-32   transform  translate-x-8  -translate-y-8   "
-          />
-          <img
-            src={avatar}
-            alt=""
-            className=" object-cover w-60 h-60 relative rounded-full border-8 border-white ml-16 z-10 "
-          />
-        </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    return 'HI';
+  }
 };
 
 export default Header;
