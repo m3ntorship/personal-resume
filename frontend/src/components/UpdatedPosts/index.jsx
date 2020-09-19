0155010a54734c96e8563f4307d0b469d27c7ee8
eslamhesham <esl2mhesham@gmail.com>
2020-09-19 04:05:30 +0200

    Fetch data from the backend, useState & useEffect to handle the data, use if condiont & handle the failure case, looping through data using map,adjusting the layouts with grid

--- frontend/src/components/UpdatedPosts/index.jsx
+++ frontend/src/components/UpdatedPosts/index.jsx
@@ -1,66 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { API } from '../../modules/apis';
 import { Heading, HEADING_OPTIONS } from '../shared/heading';
 // Import Icons
 import Heart from './heart-regular.svg';
 import Comment from './comment-regular.svg';
 export const UpdatedPosts = () => {
-  return (
-    <div className="w-main-container m-auto ">
-      <Heading
-        as="h2"
-        color={HEADING_OPTIONS.COLOR.PRIMARY}
-        className="mb-12 text-center"
-      >
-        Get Everyday Updates
-      </Heading>
-      <div className="bg-c400 w-1/3 p-6">
+  const [data, setData] = useState();
+  useEffect(() => {
+    API('/updatedPosts').then(({ data }) => {
+      setData(data);
+      console.log(data);
+    });
+  }, []);
+
+  if (data) {
+    return (
+      <div className="w-main-container m-auto grid gap-x-6 grid-cols-3">
         <Heading
-          as="h4"
-          fontSize={HEADING_OPTIONS.FONT_SIZES.XSMALL}
-          hasLineHeight={false}
+          as="h2"
+          color={HEADING_OPTIONS.COLOR.PRIMARY}
+          className="mb-12 text-center  col-start-1 col-end-4"
         >
-          Meet Inclusive Front-End Design Patterns A New Smashing Book
+          {data.postsHeader}
         </Heading>
-        <img
-          src="https://picsum.photos/500/400"
-          alt="Post update"
-          className="rounded-md my-8"
-        />
-        <div className="flex justify-between items-center">
-          <figure className="flex items-center ">
-            <img
-              src="https://picsum.photos/300/200"
-              alt="Profile"
-              className="w-10 h-10 inline rounded-full mr-4"
-            />
-            <figcaption className="inline">Eslam Hesham</figcaption>
-          </figure>
-          <div>
-            <ul className="flex">
-              <li>
-                <a href="/">
+        {data.posts.map(({ title, userName, likes, comments, id }) => {
+          return (
+            <div className="bg-c400 p-6 block" key={id}>
+              <Heading
+                as="h4"
+                fontSize={HEADING_OPTIONS.FONT_SIZES.XSMALL}
+                hasLineHeight={false}
+              >
+                {title}
+              </Heading>
+              <img
+                src="https://picsum.photos/500/400"
+                alt="Post update"
+                className="rounded-md my-8"
+              />
+              <div className="flex justify-between items-center">
+                <figure className="flex items-center ">
                   <img
-                    src={Heart}
-                    alt="Heart Icon"
-                    className="w-4 inline ml-1"
+                    src="https://picsum.photos/300/200"
+                    alt="Profile"
+                    className="w-10 h-10 inline rounded-full mr-4"
                   />
-                </a>
-                <span className="ml-1">15</span>
-              </li>
-              <li className="ml-10">
-                <a href="/">
-                  <img
-                    src={Comment}
-                    alt="Comment Icon"
-                    className="w-4 inline ml-1"
-                  />
-                </a>
-                <span className="inline ml-1">20</span>
-              </li>
-            </ul>
-          </div>
-        </div>
+                  <figcaption className="inline">{userName}</figcaption>
+                </figure>
+                <div>
+                  <ul className="flex">
+                    <li>
+                      <a href="/">
+                        <img
+                          src={Heart}
+                          alt="Heart Icon"
+                          className="w-4 inline ml-1"
+                        />
+                      </a>
+                      <span className="ml-1">{likes}</span>
+                    </li>
+                    <li className="ml-10">
+                      <a href="/">
+                        <img
+                          src={Comment}
+                          alt="Comment Icon"
+                          className="w-4 inline ml-1"
+                        />
+                      </a>
+                      <span className="inline ml-1">{comments}</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </div>
-  );
+    );
+  } else {
+    return 'Loading Broken';
+  }
 };
