3f02d61ad4747767ed1f43891e25124a5a253b9b
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-09-20 01:34:57 +0200

    add navbar component in the header component and adjust it and remove it from app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,6 @@ export default class App extends React.Component {
     return (
       <div>
         <Header />
-        <Navbar />
         {/* <Prototype /> */}
         <AboutUs />
         <BestExpertiseSection />
