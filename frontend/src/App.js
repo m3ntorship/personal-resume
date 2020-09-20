de4b4225c62d9955da692226101c015b7dfd80a5
eslamhesham <esl2mhesham@gmail.com>
2020-09-20 03:27:25 +0200

    import footer at app file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import BestExpertiseSection from './components/BestExpertiseSection/BestExpertis
 import Gallery from '../src/components/GalleryComponent/Gallery';
 import { Navbar } from './components/Navbar';
 import { UpdatedPosts } from './components/UpdatedPosts';
+import { Footer } from './components/Footer';
 export default class App extends React.Component {
   render() {
     return (
@@ -21,6 +22,7 @@ export default class App extends React.Component {
         <Gallery />
         <EstimateSection />
         <UpdatedPosts />
+        <Footer />
       </div>
     );
   }
