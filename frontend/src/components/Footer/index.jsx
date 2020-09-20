23a7c82dae17a7db05c0b81d7037b01a22c25b89
eslamhesham <esl2mhesham@gmail.com>
2020-09-20 13:55:12 +0200

    Adding footer data to backend,import API & useState to handle the data, uploading logo in images file

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import Logo from './Logo.png';
+import React, { useState, useEffect } from 'react';
+import { API } from '../../modules/apis';
 export const Footer = () => {
-  return (
-    <footer className="w-main-container m-auto flex flex-row justify-between items-center my-8">
-      <div className="mx-12">
-        <a href="/">
-          <img src={Logo} alt="Logo" className="w-16" />
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    API('/footer').then(({ data }) => {
+      setData(data);
+    });
+  }, []);
+  if (data) {
+    return (
+      <footer className="w-main-container m-auto flex flex-row justify-between items-center my-8">
+        <div className="mx-12">
+          <a href="/">
+            <img src={data.logo} alt="Logo" className="w-16" />
+          </a>
+        </div>
+        <a
+          href={data.topUrl}
+          className="text-md font-medium text-c100 flex items-center align-middle"
+        >
+          Back To Top <span className="font-bold mx-1">^</span>
         </a>
-      </div>
-      <a
-        href="/"
-        className="text-md font-medium text-c100 flex items-center align-middle"
-      >
-        Back To Top <span className="font-bold mx-1">^</span>
-      </a>
-      <span className="text-md text-c300">
-        Copyrights By&copy;Example - 2020
-      </span>
-    </footer>
-  );
+        <span className="text-md text-c300">
+          Copyrights By&copy;Example - 2020
+        </span>
+      </footer>
+    );
+  } else {
+    return 'Data Broken';
+  }
 };
