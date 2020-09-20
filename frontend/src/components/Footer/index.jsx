bfb9b9ca5749faeda36423ad114e3d2e45ce4940
eslamhesham <esl2mhesham@gmail.com>
2020-09-20 13:10:33 +0200

    rebuild the footer, created the HTML structure,added the styles, uploading the logo,delete the old photo

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,72 +1,19 @@
 import React from 'react';
-import { Heading, HEADING_OPTIONS } from '../shared/heading';
-import { Button } from '../shared/button';
-import contactPic from './contact.svg';
+import Logo from './Logo.png';
 export const Footer = () => {
   return (
-    <footer className="w-full bg-c400 py-20 my-4">
-      <div className="w-main-container m-auto grid grid-cols-2 gap-16">
-        <figure>
-          <img src={contactPic} alt="Contact Us" className="w-full h-full" />
-        </figure>
-        <div>
-          <form>
-            <Heading
-              as="h2"
-              color={HEADING_OPTIONS.COLOR.PRIMARY}
-              className="mb-12"
-            >
-              Get In Touch
-            </Heading>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <input
-                  className="appearance-none block w-full bg-c400 border-b-2 border-c300 py-3 px-4 mb-3  focus:outline-none "
-                  type="text"
-                  placeholder="Enter Name"
-                />
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <input
-                  className="appearance-none block w-full  bg-c400 border-b-2 border-c300 py-3 px-4  focus:outline-none "
-                  type="email"
-                  placeholder="Enter Email"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <input
-                  className="appearance-none block w-full  bg-c400 border-b-2 border-c300 py-3 px-4 mb-3 focus:outline-none  "
-                  type="text"
-                  placeholder="Subject"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3 ">
-                <textarea
-                  className=" w-full resize-none bg-c400 border-b-2 border-c300 p-4 mb-3 focus:outline-none "
-                  type="text"
-                  placeholder="ًWrite your message"
-                  rows="8"
-                />
-              </div>
-            </div>
-            <div className="flex items-end justify-end">
-              <div>
-                <Button
-                  rounded={false}
-                  customClassNames="my-4 block w-56 h-16 uppercase"
-                  bgColor="c100"
-                >
-                  Send message
-                </Button>
-              </div>
-            </div>
-          </form>
-        </div>
+    <footer className="w-main-container m-auto flex flex-row justify-between items-center my-8">
+      <div className="mx-12">
+        <a href="/">
+          <img src={Logo} alt="Logo" className="w-16" />
+        </a>
       </div>
+      <a href="/" className="text-lg font-bold text-c100">
+        Back To Top ^
+      </a>
+      <span className="text-md text-c300">
+        Copyrights By&copy;Example - 2020
+      </span>
     </footer>
   );
 };
