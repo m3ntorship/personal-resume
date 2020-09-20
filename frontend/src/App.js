73ea757a7559d183abd6fac04af5b24e6858470f
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-09-20 02:03:26 +0200

    remove api importing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import AboutUs from './components/AboutUsSection/AboutUs';
 import EstimateSection from './components/EstimateSection';
 import BestExpertiseSection from './components/BestExpertiseSection/BestExpertiseSection';
 // import { Prototype } from './components/TestComponent';
-// import { API } from './modules/apis';
 import Gallery from '../src/components/GalleryComponent/Gallery';
 import { UpdatedPosts } from './components/UpdatedPosts';
 export default class App extends React.Component {
