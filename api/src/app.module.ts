3ec19a6c83124cc18631033bda8850840a150b8d
MustafaMadkour <madkour.dev@gmail.com>
2020-09-24 17:35:58 +0200

    header-section api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,9 +6,10 @@ import { LinksModule } from './links/links.module';
 import { AboutModule } from './about/about.module';
 import { MongooseModule } from  '@nestjs/mongoose';
 import { HttpErrorFilter } from './shared/http-error.filter';
+import { HeaderModule } from './header/header.module';
 
 @Module({
-  imports: [LinksModule, AboutModule, MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [LinksModule, AboutModule, MongooseModule.forRoot(process.env.MONGO_URI), HeaderModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_FILTER,
